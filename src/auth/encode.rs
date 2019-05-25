e0a86045a049341fc9af63f19942eb7fa36ea3cb
王宇逸 <Strawberry_Str@hotmail.com>
2019-05-25 15:16:13 +0800

    改进算法

--- src/auth/encode.rs
+++ src/auth/encode.rs
@@ -1,31 +1,25 @@
 use std::cmp;
-use std::mem::size_of;
 use std::ptr::copy_nonoverlapping;
 use std::string::String;
 use std::vec::Vec;
 
-const BASE64N: &[u8] =
-    "LVoJPiCN2R8G90yg+hmFHuacZ1OWMnrsSTXkYpUq/3dlbfKwv6xztjI7DeBE45QA".as_bytes();
+const BASE64N: &[u8] = b"LVoJPiCN2R8G90yg+hmFHuacZ1OWMnrsSTXkYpUq/3dlbfKwv6xztjI7DeBE45QA";
+const BASE64PAD: u8 = b'=';
 
 pub fn base64(t: &[u8]) -> String {
     let a = t.len();
     let len = (a + 2) / 3 * 4;
     let mut u = vec![b'\0'; len];
-    let r = b'=';
     let mut ui = 0;
     for o in (0..a).step_by(3) {
-        let mut h = (t[o] as u32) << 16;
-        if o + 1 < a {
-            h += (t[o + 1] as u32) << 8;
-        }
-        if o + 2 < a {
-            h += t[o + 2] as u32;
-        }
+        let h = ((t[o] as u32) << 16)
+            + (if o + 1 < a { (t[o + 1] as u32) << 8 } else { 0 })
+            + (if o + 2 < a { t[o + 2] as u32 } else { 0 });
         for i in 0..4 {
             if o * 8 + i * 6 > a * 8 {
-                u[ui] = r;
+                u[ui] = BASE64PAD;
             } else {
-                u[ui] = BASE64N[(h >> 6 * (3 - i) & 0x3F) as usize];
+                u[ui] = BASE64N[(h >> (6 * (3 - i)) & 0x3F) as usize];
             }
             ui += 1;
         }
@@ -44,7 +38,7 @@ fn s(a: &[u8], b: bool) -> Vec<u32> {
         v = vec![0; cmp::max(n, 4)];
     }
     unsafe {
-        copy_nonoverlapping(a.as_ptr(), v.as_mut_ptr() as *mut u8, c * size_of::<u8>());
+        copy_nonoverlapping(a.as_ptr(), v.as_mut_ptr() as *mut u8, c);
     }
     v
 }
@@ -59,20 +53,12 @@ fn l(a: &[u32], b: bool) -> Vec<u8> {
         }
         c = m;
     }
-    let n = d << 2;
+    let n = if b { c as usize } else { d << 2 };
     let mut aa = vec![b'\0'; n];
     unsafe {
-        copy_nonoverlapping(
-            a.as_ptr() as *const u8,
-            aa.as_mut_ptr(),
-            n * size_of::<u8>(),
-        );
+        copy_nonoverlapping(a.as_ptr() as *const u8, aa.as_mut_ptr(), n);
     }
-    if b {
-        aa[0..(c as usize)].to_vec()
-    } else {
-        aa
-    }
+    aa
 }
 
 pub fn xencode(st: &str, key: &str) -> Vec<u8> {
@@ -87,15 +73,14 @@ pub fn xencode(st: &str, key: &str) -> Vec<u8> {
     let q = 6 + 52 / (n + 1);
     let mut d: u32 = 0;
     for _i in 0..q {
-        let (dd, _) = d.overflowing_add(0x9E3779B9);
-        d = dd;
+        d = d.wrapping_add(0x9E3779B9);
         let e = (d >> 2) & 3;
         for p in 0..=n {
             y = v[(p + 1) % (n + 1)];
-            let m = (z >> 5) ^ (y << 2);
-            let (m, _) = m.overflowing_add((y >> 3) ^ (z << 4) ^ (d ^ y));
-            let (m, _) = m.overflowing_add(k[(((p & 3) as u32) ^ e) as usize] ^ z);
-            let (m, _) = m.overflowing_add(v[p]);
+            let mut m = (z >> 5) ^ (y << 2);
+            m = m.wrapping_add((y >> 3) ^ (z << 4) ^ (d ^ y));
+            m = m.wrapping_add(k[(((p & 3) as u32) ^ e) as usize] ^ z);
+            m = m.wrapping_add(v[p]);
             v[p] = m;
             z = v[p];
         }
