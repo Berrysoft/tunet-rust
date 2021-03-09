de0006e73ac0477379596712620385a0d7bb469c
Berrysoft <Strawberry_Str@hotmail.com>
2021-03-09 15:27:02 +0800

    Fix dependency for unix.

--- tunet-rust/src/suggest/mod.rs
+++ tunet-rust/src/suggest/mod.rs
@@ -1,17 +1,19 @@
 use crate::*;
+use cfg_if::cfg_if;
 
 mod ping;
 
-#[cfg(feature = "winrt")]
-mod winrt;
+cfg_if! {
+    if #[cfg(windows)] {
+        mod winrt;
 
-#[cfg(feature = "winrt")]
-pub fn suggest(client: &HttpClient) -> NetState {
-    match winrt::suggest().unwrap_or(NetState::Unknown) {
-        NetState::Unknown => ping::suggest(client),
-        state => state,
+        pub fn suggest(client: &HttpClient) -> NetState {
+            match winrt::suggest().unwrap_or(NetState::Unknown) {
+                NetState::Unknown => ping::suggest(client),
+                state => state,
+            }
+        }
+    } else {
+        pub use ping::suggest;
     }
 }
-
-#[cfg(not(feature = "winrt"))]
-pub use ping::suggest;
