43eba834240e53149f3637c303e2b21765360d55
Berrysoft <Strawberry_Str@hotmail.com>
2021-03-10 21:41:58 +0800

    Add libiw support for linux.

---
+++ tunet-rust/src/suggest/libiw.rs
@@ -0,0 +1,52 @@
+use super::SUGGEST_SSID_MAP;
+use crate::*;
+use libiw_bindings::*;
+use std::ffi::CStr;
+use std::mem::MaybeUninit;
+use std::os::raw::{c_char, c_int};
+
+#[repr(transparent)]
+struct Socket(c_int);
+
+impl Drop for Socket {
+    fn drop(&mut self) {
+        if self.0 >= 0 {
+            unsafe { iw_sockets_close(self.0) }
+        }
+    }
+}
+
+unsafe extern "C" fn enum_handler(
+    skfd: c_int,
+    ifname: *mut c_char,
+    args: *mut *mut c_char,
+    _count: c_int,
+) -> c_int {
+    #[cfg(debug_assertions)]
+    assert_eq!(_count, 1);
+    let ssids = ((*args) as *mut Vec<String>).as_mut().unwrap_unchecked();
+    let mut info = MaybeUninit::uninit().assume_init();
+    iw_get_basic_config(skfd, ifname, &mut info);
+    let mut dest = [0; 129];
+    iw_essid_escape(dest.as_mut_ptr(), info.essid.as_ptr(), info.essid_len);
+    ssids.push(CStr::from_ptr(dest.as_ptr()).to_string_lossy().into_owned());
+    0
+}
+
+pub fn suggest() -> NetState {
+    unsafe {
+        let skfd = Socket(iw_sockets_open());
+        if skfd.0 < 0 {
+            return NetState::Unknown;
+        }
+        let mut ssids: Vec<String> = vec![];
+        let mut args = [&mut ssids as *mut _ as *mut c_char];
+        iw_enum_devices(skfd.0, Some(enum_handler), args.as_mut_ptr(), 1);
+        for ssid in ssids {
+            if let Some(s) = SUGGEST_SSID_MAP.get(ssid.as_str()) {
+                return *s;
+            }
+        }
+    }
+    NetState::Unknown
+}
--- tunet-rust/src/suggest/mod.rs
+++ tunet-rust/src/suggest/mod.rs
@@ -1,47 +1,45 @@
-use cfg_if::cfg_if;
-
 mod ping;
 
-cfg_if! {
-    if #[cfg(any(target_os = "windows", target_os = "macos"))] {
-        use crate::*;
-        use std::collections::BTreeMap;
-        use lazy_static::lazy_static;
+use crate::*;
+use lazy_static::lazy_static;
+use std::collections::BTreeMap;
 
-        lazy_static! {
-            static ref SUGGEST_SSID_MAP: BTreeMap<&'static str, NetState> = {
-                let mut map = BTreeMap::new();
-                map.insert("Tsinghua", NetState::Auth4);
-                map.insert("Tsinghua-5G", NetState::Auth4);
-                map.insert("Tsinghua-IPv4", NetState::Auth4);
-                map.insert("Tsinghua-IPv6", NetState::Auth6);
-                map.insert("Tsinghua-Secure", NetState::Net);
-                map.insert("Wifi.郑裕彤讲堂", NetState::Net);
-                map
-            };
-        }
+lazy_static! {
+    static ref SUGGEST_SSID_MAP: BTreeMap<&'static str, NetState> = {
+        let mut map = BTreeMap::new();
+        map.insert("Tsinghua", NetState::Auth4);
+        map.insert("Tsinghua-5G", NetState::Auth4);
+        map.insert("Tsinghua-IPv4", NetState::Auth4);
+        map.insert("Tsinghua-IPv6", NetState::Auth6);
+        map.insert("Tsinghua-Secure", NetState::Net);
+        map.insert("Wifi.郑裕彤讲堂", NetState::Net);
+        map
+    };
+}
 
-        #[cfg(target_os = "windows")]
-        mod winrt;
+#[cfg(target_os = "windows")]
+mod winrt;
 
-        #[cfg(target_os = "macos")]
-        mod macos;
+#[cfg(target_os = "macos")]
+mod macos;
 
-        mod platform {
-            #[cfg(target_os = "windows")]
-            pub use super::winrt::*;
+#[cfg(target_os = "linux")]
+mod libiw;
 
-            #[cfg(target_os = "macos")]
-            pub use super::macos::*;
-        }
+mod platform {
+    #[cfg(target_os = "windows")]
+    pub use super::winrt::*;
 
-        pub fn suggest(client: &HttpClient) -> NetState {
-            match platform::suggest() {
-                NetState::Unknown => ping::suggest(client),
-                state => state,
-            }
-        }
-    } else {
-        pub use ping::suggest;
+    #[cfg(target_os = "macos")]
+    pub use super::macos::*;
+
+    #[cfg(target_os = "linux")]
+    pub use super::libiw::*;
+}
+
+pub fn suggest(client: &HttpClient) -> NetState {
+    match platform::suggest() {
+        NetState::Unknown => ping::suggest(client),
+        state => state,
     }
 }
