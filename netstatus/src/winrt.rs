0aac266884e0983b8ce96f5866aee4d62ee01d24
Berrysoft <Strawberry_Str@hotmail.com>
2021-03-19 22:04:04 +0800

    Bump windows-rs

--- netstatus/src/winrt.rs
+++ netstatus/src/winrt.rs
@@ -4,18 +4,19 @@ use winrt_bindings::windows::networking::connectivity::*;
 fn current_impl() -> windows::Result<NetStatus> {
     let profile = NetworkInformation::get_internet_connection_profile()?;
     let cl = profile.get_network_connectivity_level()?;
-    if cl == NetworkConnectivityLevel::None {
-        Ok(NetStatus::Unknown)
-    } else {
-        if profile.is_wlan_connection_profile()? {
-            let ssid = profile
-                .wlan_connection_profile_details()?
-                .get_connected_ssid()?;
-            Ok(NetStatus::Wlan(ssid.to_string_lossy()))
-        } else if profile.is_wwan_connection_profile()? {
-            Ok(NetStatus::Wwan)
-        } else {
-            Ok(NetStatus::Lan)
+    match cl {
+        NetworkConnectivityLevel::None => Ok(NetStatus::Unknown),
+        _ => {
+            if profile.is_wlan_connection_profile()? {
+                let ssid = profile
+                    .wlan_connection_profile_details()?
+                    .get_connected_ssid()?;
+                Ok(NetStatus::Wlan(ssid.to_string_lossy()))
+            } else if profile.is_wwan_connection_profile()? {
+                Ok(NetStatus::Wwan)
+            } else {
+                Ok(NetStatus::Lan)
+            }
         }
     }
 }
