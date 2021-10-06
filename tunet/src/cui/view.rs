feeac1bbe1cff3f7e4cb56ba02cbfc58373ee055
Berrysoft <Strawberry_Str@hotmail.com>
2021-10-06 20:18:27 +0800

    Adjust view.

--- tunet/src/cui/view.rs
+++ tunet/src/cui/view.rs
@@ -28,10 +28,13 @@ pub fn draw<B: Backend>(m: &Model, f: &mut Frame<B>) {
         .direction(Direction::Vertical)
         .constraints([Constraint::Length(6), Constraint::Percentage(100)])
         .split(chunks[0]);
+
+    let subtitle_style = Style::default().fg(Color::Cyan);
+
     let graph = if let Some(flux) = &m.flux {
         Paragraph::new(vec![
             Spans::from(vec![
-                Span::styled("用户 ", Style::default().fg(Color::Cyan)),
+                Span::styled("用户 ", subtitle_style),
                 Span::styled(
                     &flux.username,
                     Style::default()
@@ -40,7 +43,7 @@ pub fn draw<B: Backend>(m: &Model, f: &mut Frame<B>) {
                 ),
             ]),
             Spans::from(vec![
-                Span::styled("流量 ", Style::default().fg(Color::Cyan)),
+                Span::styled("流量 ", subtitle_style),
                 Span::styled(
                     flux.flux.to_string(),
                     Style::default()
@@ -49,14 +52,14 @@ pub fn draw<B: Backend>(m: &Model, f: &mut Frame<B>) {
                 ),
             ]),
             Spans::from(vec![
-                Span::styled("时长 ", Style::default().fg(Color::Cyan)),
+                Span::styled("时长 ", subtitle_style),
                 Span::styled(
                     FmtDuration(flux.online_time).to_string(),
                     Style::default().fg(Color::Green),
                 ),
             ]),
             Spans::from(vec![
-                Span::styled("余额 ", Style::default().fg(Color::Cyan)),
+                Span::styled("余额 ", subtitle_style),
                 Span::styled(flux.balance.to_string(), Style::default().fg(Color::Yellow)),
             ]),
         ])
@@ -72,7 +75,7 @@ pub fn draw<B: Backend>(m: &Model, f: &mut Frame<B>) {
             .map(|u| {
                 ListItem::new(Text::from(vec![
                     Spans::from(vec![
-                        Span::styled("IP 地址  ", Style::default().fg(Color::Cyan)),
+                        Span::styled("IP 地址  ", subtitle_style),
                         Span::styled(
                             u.address.to_string(),
                             Style::default()
@@ -81,14 +84,14 @@ pub fn draw<B: Backend>(m: &Model, f: &mut Frame<B>) {
                         ),
                     ]),
                     Spans::from(vec![
-                        Span::styled("登录时间 ", Style::default().fg(Color::Cyan)),
+                        Span::styled("登录时间 ", subtitle_style),
                         Span::styled(
                             FmtDateTime(u.login_time).to_string(),
                             Style::default().fg(Color::Green),
                         ),
                     ]),
                     Spans::from(vec![
-                        Span::styled("MAC 地址 ", Style::default().fg(Color::Cyan)),
+                        Span::styled("MAC 地址 ", subtitle_style),
                         Span::styled(
                             u.mac_address.map(|a| a.to_string()).unwrap_or_default(),
                             Style::default().fg(Color::LightCyan),
@@ -114,7 +117,7 @@ pub fn draw<B: Backend>(m: &Model, f: &mut Frame<B>) {
 
     let mut details = vec![];
     let mut flux = 0u64;
-    for d in 1u32..max_day {
+    for d in 1u32..=max_day {
         if let Some(f) = details_group.get(&d) {
             flux += *f;
         }
@@ -129,7 +132,8 @@ pub fn draw<B: Backend>(m: &Model, f: &mut Frame<B>) {
         .unwrap_or_default()
         .max(flux_g)
         .max(1.0)
-        * 1.1) as usize;
+        * 1.1)
+        .ceil() as usize;
 
     let dataset = Dataset::default()
         .marker(tui::symbols::Marker::Dot)
@@ -141,10 +145,9 @@ pub fn draw<B: Backend>(m: &Model, f: &mut Frame<B>) {
         .x_axis(
             Axis::default()
                 .title(Span::from("日期"))
-                .style(Style::default().fg(Color::White))
                 .bounds([1.0, max_day as f64])
                 .labels(
-                    (1..max_day)
+                    (1..=max_day)
                         .into_iter()
                         .map(|d| Span::from(d.to_string()))
                         .collect(),
@@ -153,26 +156,30 @@ pub fn draw<B: Backend>(m: &Model, f: &mut Frame<B>) {
         .y_axis(
             Axis::default()
                 .title(Span::from("流量"))
-                .style(Style::default().fg(Color::White))
                 .bounds([0.0, max_flux as f64])
-                .labels(
-                    (0..3)
-                        .map(|f| Span::from((max_flux as f64 / 2.0 * f as f64).to_string()))
-                        .collect(),
-                ),
+                .labels((0..=max_flux).map(|f| Span::from(f.to_string())).collect()),
         )
         .block(Block::default().title("流量详情").borders(Borders::all()));
     f.render_widget(chart, chunks[1]);
 
+    let key_style = Style::default().bg(Color::Black).fg(Color::White);
+
     let status = Paragraph::new(Spans::from(vec![
-        Span::raw("F1 登录  F2 注销  F3 刷新流量  F4 刷新在线  F5 刷新图表"),
-        Span::raw("    "),
+        Span::styled("F1", key_style),
+        Span::raw("登录"),
+        Span::styled("F2", key_style),
+        Span::raw("注销"),
+        Span::styled("F3", key_style),
+        Span::raw("刷新流量"),
+        Span::styled("F4", key_style),
+        Span::raw("刷新在线"),
+        Span::styled("F5", key_style),
+        Span::raw("刷新图表"),
+        Span::styled("    ", key_style),
         Span::raw(m.log.as_ref().map(|s| s.as_ref()).unwrap_or("")),
-        Span::raw("    "),
-        Span::raw(if m.online { "正在刷新在线" } else { "" }),
-        Span::raw("    "),
-        Span::raw(if m.detail { "正在刷新图表" } else { "" }),
+        Span::raw(if m.online { "|正在刷新在线" } else { "" }),
+        Span::raw(if m.detail { "|正在刷新图表" } else { "" }),
     ]))
-    .block(Block::default().style(Style::default().bg(Color::White).fg(Color::Black)));
+    .block(Block::default().style(Style::default().bg(Color::LightCyan).fg(Color::Black)));
     f.render_widget(status, global_chunks[1]);
 }
