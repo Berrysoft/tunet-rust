dacf4ac50028d1b19e468f233445b04d88918290
Berrysoft <Strawberry_Str@hotmail.com>
2021-10-17 00:01:25 +0800

    Move info page.

---
+++ tunet-gui/src/info.rs
@@ -0,0 +1,246 @@
+use crate::*;
+use tokio::task::JoinHandle;
+
+pub enum InfoMsg {
+    Refresh,
+    Log(String),
+    Flux(NetFlux),
+    FetchFlux,
+    Tick,
+    State(NetState),
+}
+
+pub struct InfoModel {
+    pub log: String,
+    pub flux: NetFlux,
+    pub state: NetState,
+
+    timer: Option<JoinHandle<()>>,
+}
+
+impl InfoModel {
+    async fn fetch_flux(s: Sender<InfoMsg>) {
+        async fn fetch() -> Result<NetFlux> {
+            clients::tunet().await?.flux().await
+        }
+
+        match fetch().await {
+            Ok(flux) => {
+                send!(s, InfoMsg::Flux(flux));
+                send!(s, InfoMsg::Log(String::new()));
+            }
+            Err(e) => send!(s, InfoMsg::Log(e.to_string())),
+        }
+    }
+}
+
+impl Model for InfoModel {
+    type Msg = InfoMsg;
+    type Widgets = InfoWidgets;
+    type Components = ();
+}
+
+impl ComponentUpdate<MainModel> for InfoModel {
+    fn init_model(parent_model: &MainModel) -> Self {
+        Self {
+            log: String::default(),
+            flux: NetFlux::default(),
+            state: NetState::Auto,
+            timer: None,
+        }
+    }
+
+    fn update(
+        &mut self,
+        msg: InfoMsg,
+        components: &(),
+        sender: Sender<InfoMsg>,
+        parent_sender: Sender<MainMsg>,
+    ) {
+        match msg {
+            InfoMsg::Refresh => {
+                if self.state == NetState::Auto {
+                    tokio::spawn(async move {
+                        let state = tunet_rust::suggest::suggest(&clients::HTTP_CLIENT).await;
+                        send!(sender, InfoMsg::State(state));
+                    });
+                }
+            }
+            InfoMsg::Log(s) => self.log = s,
+            InfoMsg::Flux(f) => {
+                self.flux = f.clone();
+                if let Some(h) = self.timer.take() {
+                    h.abort();
+                }
+                if !f.username.is_empty() {
+                    self.timer = Some(tokio::spawn(async move {
+                        let mut interval = tokio::time::interval(std::time::Duration::from_secs(1));
+                        loop {
+                            interval.tick().await;
+                            send!(sender, InfoMsg::Tick);
+                        }
+                    }));
+                }
+            }
+            InfoMsg::FetchFlux => {
+                tokio::spawn(Self::fetch_flux(sender));
+            }
+            InfoMsg::Tick => {
+                self.flux.online_time =
+                    Duration(self.flux.online_time.0 + NaiveDuration::seconds(1));
+            }
+            InfoMsg::State(s) => {
+                self.state = s;
+                tokio::spawn(async move {
+                    clients::replace_state(s).await;
+                    send!(sender, InfoMsg::FetchFlux);
+                });
+            }
+        }
+    }
+}
+
+#[relm4_macros::widget(pub)]
+impl Widgets<InfoModel, MainModel> for InfoWidgets {
+    view! {
+        gtk::Box {
+            set_orientation: gtk::Orientation::Vertical,
+            set_margin_all: 5,
+            set_spacing: 5,
+
+            append = &gtk::Overlay {
+                set_height_request: 300,
+
+                add_overlay = &gtk::Box {
+                    set_orientation: gtk::Orientation::Vertical,
+                    set_spacing: 5,
+                    set_halign: gtk::Align::Center,
+                    set_valign: gtk::Align::Center,
+
+                    append = &gtk::Label {
+                        set_xalign: 0.,
+                        set_label: watch! { &format!("用户：{}", model.flux.username) },
+                    },
+                    append = &gtk::Label {
+                        set_xalign: 0.,
+                        set_label: watch! { &format!("流量：{}", model.flux.flux) },
+                    },
+                    append = &gtk::Label {
+                        set_xalign: 0.,
+                        set_label: watch! { &format!("时长：{}", model.flux.online_time) },
+                    },
+                    append = &gtk::Label {
+                        set_xalign: 0.,
+                        set_label: watch! { &format!("余额：{}", model.flux.balance) },
+                    },
+                },
+
+                set_child: area = Some(&gtk::DrawingArea) {
+                    set_vexpand: true,
+                    set_hexpand: true,
+
+                    connect_resize(sender) => move |_, _, _| {
+                        send!(sender, InfoMsg::Refresh);
+                    },
+                }
+            },
+
+            append = &gtk::ComboBoxText {
+                append_text: "Net",
+                append_text: "Auth4",
+                append_text: "Auth6",
+
+                set_active: watch! {
+                    match model.state {
+                        NetState::Net => Some(0),
+                        NetState::Auth4 => Some(1),
+                        NetState::Auth6 => Some(2),
+                        _ => None
+                    }
+                },
+
+                connect_changed(sender) => move |c| {
+                    let state = match c.active() {
+                        Some(i) => match i {
+                            0 => NetState::Net,
+                            1 => NetState::Auth4,
+                            2 => NetState::Auth6,
+                            _ => unreachable!(),
+                        },
+                        None => NetState::Unknown,
+                    };
+                    send!(sender, InfoMsg::State(state));
+                }
+            },
+
+            append = &gtk::Label {
+                set_wrap: true,
+                set_label: watch! { &model.log },
+            },
+
+            append = &gtk::Box {
+                set_orientation: gtk::Orientation::Horizontal,
+                set_spacing: 5,
+                set_homogeneous: true,
+                set_halign: gtk::Align::Fill,
+
+                append = &gtk::Button {
+                    set_label: "登录",
+                },
+                append = &gtk::Button {
+                    set_label: "注销",
+                },
+                append = &gtk::Button {
+                    set_label: "刷新",
+                    connect_clicked(sender) => move |_| {
+                        send!(sender, InfoMsg::FetchFlux);
+                    },
+                },
+            },
+        }
+    }
+
+    additional_fields! {
+        handler: DrawHandler,
+    }
+
+    fn post_init() {
+        let mut handler = DrawHandler::new().unwrap();
+        handler.init(&area);
+    }
+
+    fn manual_view() {
+        use std::f64::consts::PI;
+
+        let width = self.area.width() as f64;
+        let height = self.area.height() as f64;
+
+        let radius = width.min(height) * 0.4;
+
+        let context = self.handler.get_context().unwrap();
+
+        context.save().unwrap();
+        context.set_operator(gtk::cairo::Operator::Clear);
+        context.paint().unwrap();
+        context.restore().unwrap();
+
+        context.set_line_width(radius * 0.2);
+
+        let max_flux = model.flux.balance.0 + 50.;
+
+        let flux_angle = PI / 2. + (model.flux.flux.to_gb() / max_flux * 2. * PI).min(PI * 2.);
+        let free_angle = PI / 2. + (50. / max_flux * 2. * PI).min(PI * 2.);
+
+        context.set_source_rgba(0., 120. / 255., 215. / 255., 0.55);
+        context.arc(width / 2., height / 2., radius, free_angle, PI / 2.);
+        context.stroke().unwrap();
+
+        context.set_source_rgba(0., 120. / 255., 215. / 255., 0.75);
+        context.arc(width / 2., height / 2., radius, flux_angle, free_angle);
+        context.stroke().unwrap();
+
+        context.set_source_rgb(0., 120. / 255., 215. / 255.);
+        context.arc(width / 2., height / 2., radius, PI / 2., flux_angle);
+        context.stroke().unwrap();
+    }
+}
--- tunet-gui/src/main.rs
+++ tunet-gui/src/main.rs
@@ -2,10 +2,10 @@
 
 use gtk::prelude::*;
 use relm4::{drawing::*, *};
-use tokio::task::JoinHandle;
 use tunet_rust::*;
 
 mod clients;
+mod info;
 
 #[tokio::main(worker_threads = 4)]
 async fn main() -> Result<()> {
@@ -17,97 +17,30 @@ async fn main() -> Result<()> {
     Ok(())
 }
 
-enum MainMsg {
-    Refresh,
-    Log(String),
-    Flux(NetFlux),
-    StartFlux,
-    Tick,
-    ChooseState(NetState),
-}
+enum MainMsg {}
 
 #[derive(Debug)]
-struct MainModel {
-    pub log: String,
-    pub flux: NetFlux,
-    pub state: NetState,
-
-    timer: Option<JoinHandle<()>>,
-}
+struct MainModel {}
 
 impl MainModel {
     pub fn new() -> Self {
-        Self {
-            log: String::default(),
-            flux: NetFlux::default(),
-            state: NetState::Auto,
-            timer: None,
-        }
-    }
-
-    async fn fetch_flux(s: Sender<MainMsg>) {
-        async fn fetch() -> Result<NetFlux> {
-            clients::tunet().await?.flux().await
-        }
-
-        match fetch().await {
-            Ok(flux) => {
-                send!(s, MainMsg::Flux(flux));
-                send!(s, MainMsg::Log(String::new()));
-            }
-            Err(e) => send!(s, MainMsg::Log(e.to_string())),
-        }
+        Self {}
     }
 }
 
 impl Model for MainModel {
     type Msg = MainMsg;
     type Widgets = MainWidgets;
-    type Components = ();
+    type Components = MainComponents;
 }
 
 impl AppUpdate for MainModel {
-    fn update(&mut self, msg: MainMsg, _components: &(), sender: Sender<MainMsg>) -> bool {
-        match msg {
-            MainMsg::Refresh => {
-                if self.state == NetState::Auto {
-                    tokio::spawn(async move {
-                        let state = tunet_rust::suggest::suggest(&clients::HTTP_CLIENT).await;
-                        send!(sender, MainMsg::ChooseState(state));
-                    });
-                }
-            }
-            MainMsg::Log(s) => self.log = s,
-            MainMsg::Flux(f) => {
-                self.flux = f.clone();
-                if let Some(h) = self.timer.take() {
-                    h.abort();
-                }
-                if !f.username.is_empty() {
-                    self.timer = Some(tokio::spawn(async move {
-                        let mut interval = tokio::time::interval(std::time::Duration::from_secs(1));
-                        loop {
-                            interval.tick().await;
-                            send!(sender, MainMsg::Tick);
-                        }
-                    }));
-                }
-            }
-            MainMsg::StartFlux => {
-                tokio::spawn(Self::fetch_flux(sender));
-            }
-            MainMsg::Tick => {
-                self.flux.online_time =
-                    Duration(self.flux.online_time.0 + NaiveDuration::seconds(1));
-            }
-            MainMsg::ChooseState(s) => {
-                self.state = s;
-                tokio::spawn(async move {
-                    clients::replace_state(s).await;
-                    send!(sender, MainMsg::StartFlux);
-                });
-            }
-        }
+    fn update(
+        &mut self,
+        msg: MainMsg,
+        _components: &MainComponents,
+        sender: Sender<MainMsg>,
+    ) -> bool {
         true
     }
 }
@@ -120,145 +53,23 @@ impl Widgets<MainModel, ()> for MainWidgets {
             set_default_width: 400,
             set_default_height: 400,
 
-            set_child = Some(&gtk::Box) {
-                set_orientation: gtk::Orientation::Vertical,
-                set_margin_all: 5,
-                set_spacing: 5,
-
-                append = &gtk::Overlay {
-                    set_height_request: 300,
-
-                    add_overlay = &gtk::Box {
-                        set_orientation: gtk::Orientation::Vertical,
-                        set_spacing: 5,
-                        set_halign: gtk::Align::Center,
-                        set_valign: gtk::Align::Center,
-
-                        append = &gtk::Label {
-                            set_xalign: 0.,
-                            set_label: watch! { &format!("用户：{}", model.flux.username) },
-                        },
-                        append = &gtk::Label {
-                            set_xalign: 0.,
-                            set_label: watch! { &format!("流量：{}", model.flux.flux) },
-                        },
-                        append = &gtk::Label {
-                            set_xalign: 0.,
-                            set_label: watch! { &format!("时长：{}", model.flux.online_time) },
-                        },
-                        append = &gtk::Label {
-                            set_xalign: 0.,
-                            set_label: watch! { &format!("余额：{}", model.flux.balance) },
-                        },
-                    },
-
-                    set_child: area = Some(&gtk::DrawingArea) {
-                        set_vexpand: true,
-                        set_hexpand: true,
-
-                        connect_resize(sender) => move |_, _, _| {
-                            send!(sender, MainMsg::Refresh);
-                        },
-                    }
-                },
-
-                append = &gtk::ComboBoxText {
-                    append_text: "Net",
-                    append_text: "Auth4",
-                    append_text: "Auth6",
-
-                    set_active: watch! {
-                        match model.state {
-                            NetState::Net => Some(0),
-                            NetState::Auth4 => Some(1),
-                            NetState::Auth6 => Some(2),
-                            _ => None
-                        }
-                    },
-
-                    connect_changed(sender) => move |c| {
-                        let state = match c.active() {
-                            Some(i) => match i {
-                                0 => NetState::Net,
-                                1 => NetState::Auth4,
-                                2 => NetState::Auth6,
-                                _ => unreachable!(),
-                            },
-                            None => NetState::Unknown,
-                        };
-                        send!(sender, MainMsg::ChooseState(state));
-                    }
-                },
-
-                append = &gtk::Label {
-                    set_wrap: true,
-                    set_label: watch! { &model.log },
-                },
-
-                append = &gtk::Box {
-                    set_orientation: gtk::Orientation::Horizontal,
-                    set_spacing: 5,
-                    set_homogeneous: true,
-                    set_halign: gtk::Align::Fill,
-
-                    append = &gtk::Button {
-                        set_label: "登录",
-                    },
-                    append = &gtk::Button {
-                        set_label: "注销",
-                    },
-                    append = &gtk::Button {
-                        set_label: "刷新",
-                        connect_clicked(sender) => move |_| {
-                            send!(sender, MainMsg::StartFlux);
-                        },
-                    },
-                },
-            },
+            set_child: component!(Some(components.info.root_widget())),
         }
     }
+}
 
-    additional_fields! {
-        handler: DrawHandler,
-    }
+struct MainComponents {
+    info: RelmComponent<info::InfoModel, MainModel>,
+}
 
-    fn post_init() {
-        let mut handler = DrawHandler::new().unwrap();
-        handler.init(&area);
-    }
-
-    fn manual_view() {
-        use std::f64::consts::PI;
-
-        let width = self.area.width() as f64;
-        let height = self.area.height() as f64;
-
-        let radius = width.min(height) * 0.4;
-
-        let context = self.handler.get_context().unwrap();
-
-        context.save().unwrap();
-        context.set_operator(gtk::cairo::Operator::Clear);
-        context.paint().unwrap();
-        context.restore().unwrap();
-
-        context.set_line_width(radius * 0.2);
-
-        let max_flux = model.flux.balance.0 + 50.;
-
-        let flux_angle = PI / 2. + (model.flux.flux.to_gb() / max_flux * 2. * PI).min(PI * 2.);
-        let free_angle = PI / 2. + (50. / max_flux * 2. * PI).min(PI * 2.);
-
-        context.set_source_rgba(0., 120. / 255., 215. / 255., 0.55);
-        context.arc(width / 2., height / 2., radius, free_angle, PI / 2.);
-        context.stroke().unwrap();
-
-        context.set_source_rgba(0., 120. / 255., 215. / 255., 0.75);
-        context.arc(width / 2., height / 2., radius, flux_angle, free_angle);
-        context.stroke().unwrap();
-
-        context.set_source_rgb(0., 120. / 255., 215. / 255.);
-        context.arc(width / 2., height / 2., radius, PI / 2., flux_angle);
-        context.stroke().unwrap();
+impl Components<MainModel> for MainComponents {
+    fn init_components(
+        parent_model: &MainModel,
+        parent_widgets: &MainWidgets,
+        parent_sender: Sender<MainMsg>,
+    ) -> Self {
+        Self {
+            info: RelmComponent::new(parent_model, parent_widgets, parent_sender),
+        }
     }
 }
