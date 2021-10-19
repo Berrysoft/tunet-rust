47ab51eff96eb73b126838d5aeb135952cdeba6d
Berrysoft <Strawberry_Str@hotmail.com>
2021-10-20 00:46:23 +0800

    Fix dpi awareness.

--- tunet-gui/src/main.rs
+++ tunet-gui/src/main.rs
@@ -19,9 +19,22 @@ async fn main() -> Result<()> {
     clients::init()?;
 
     gtk::init()?;
-    let app = gtk::Application::builder().build();
 
-    let model = MainModel::new(app.clone());
+    #[cfg(windows)]
+    let factor = win32::get_scale_factor();
+    #[cfg(not(windows))]
+    let factor = 1.0;
+
+    let style = gtk::CssProvider::new();
+    style.load_from_data(format!("*{{font-size:{}px;}}", 16.0 * factor).as_bytes());
+    gtk::StyleContext::add_provider_for_display(
+        &gtk::gdk::Display::default().unwrap(),
+        &style,
+        gtk::STYLE_PROVIDER_PRIORITY_APPLICATION,
+    );
+
+    let model = MainModel::new();
+    let app = gtk::Application::builder().build();
     let app = RelmApp::with_app(model, app);
     app.run();
     Ok(())
@@ -35,26 +48,16 @@ enum MainMode {
 
 enum MainMsg {
     Show,
-    Resize,
     Mode(MainMode),
 }
 
-#[allow(dead_code)]
 struct MainModel {
-    dpi: f64,
-    style: gtk::CssProvider,
-    app: gtk::Application,
     child: Option<gtk::Box>,
 }
 
 impl MainModel {
-    pub fn new(app: gtk::Application) -> Self {
-        Self {
-            dpi: 1.0,
-            style: gtk::CssProvider::new(),
-            app,
-            child: None,
-        }
+    pub fn new() -> Self {
+        Self { child: None }
     }
 }
 
@@ -76,27 +79,6 @@ impl AppUpdate for MainModel {
                 send!(sender, MainMsg::Mode(MainMode::Info));
                 components.info.send(info::InfoMsg::Show).unwrap();
                 components.detail.send(detail::DetailMsg::Show).unwrap();
-            }
-            MainMsg::Resize =>
-            {
-                #[cfg(windows)]
-                if let Some(window) = self.app.active_window() {
-                    let factor = win32::get_scale_factor(window);
-                    if self.dpi != factor {
-                        self.dpi = factor;
-                        let display = gtk::gdk::Display::default().unwrap();
-                        gtk::StyleContext::remove_provider_for_display(&display, &self.style);
-                        self.style = gtk::CssProvider::new();
-                        self.style.load_from_data(
-                            format!("*{{font-size:{}px;}}", 16.0 * factor).as_bytes(),
-                        );
-                        gtk::StyleContext::add_provider_for_display(
-                            &display,
-                            &self.style,
-                            gtk::STYLE_PROVIDER_PRIORITY_APPLICATION,
-                        );
-                    }
-                }
             }
             MainMsg::Mode(m) => match m {
                 MainMode::Info => self.child = Some(components.info.root_widget().clone()),
@@ -121,12 +103,6 @@ impl Widgets<MainModel, ()> for MainWidgets {
 
             connect_show(sender) => move |_| {
                 send!(sender, MainMsg::Show);
-            },
-            connect_default_width_notify(sender) => move |_| {
-                send!(sender, MainMsg::Resize);
-            },
-            connect_default_height_notify(sender) => move |_| {
-                send!(sender, MainMsg::Resize);
             },
         }
     }
--- tunet-gui/src/win32.rs
+++ tunet-gui/src/win32.rs
@@ -1,6 +1,5 @@
 use crate::*;
 use libloading::os::windows::*;
-use std::ffi::c_void;
 
 fn get<T>(this: &Option<Library>, symbol: &[u8]) -> Option<Symbol<T>> {
     match this {
@@ -12,27 +11,18 @@ fn get<T>(this: &Option<Library>, symbol: &[u8]) -> Option<Symbol<T>> {
     }
 }
 
-extern "C" {
-    fn gdk_win32_surface_get_handle(surface: *mut c_void) -> *mut c_void;
-}
-
-type FnGetDpiForWindow = unsafe extern "system" fn(*mut c_void) -> u32;
+type FnGetDpiForSystem = unsafe extern "system" fn() -> u32;
 
 lazy_static! {
     static ref USER32: Option<Library> =
         unsafe { Library::load_with_flags("User32.dll", LOAD_LIBRARY_SEARCH_SYSTEM32).ok() };
-    static ref GET_DPI_FOR_WINDOW: Option<Symbol<FnGetDpiForWindow>> =
-        get(&USER32, b"GetDpiForWindow\0");
+    static ref GET_DPI_FOR_SYSTEM: Option<Symbol<FnGetDpiForSystem>> =
+        get(&USER32, b"GetDpiForSystem\0");
 }
 
-pub fn get_scale_factor(wnd: impl IsA<gtk::Widget>) -> f64 {
-    if let Some(native) = wnd.native() {
-        if let Some(surface) = native.surface() {
-            if let Some(ref f) = *GET_DPI_FOR_WINDOW {
-                let hwnd = unsafe { gdk_win32_surface_get_handle(surface.as_ptr() as _) };
-                return unsafe { f(hwnd) } as f64 / 96.0;
-            }
-        }
+pub fn get_scale_factor() -> f64 {
+    if let Some(ref f) = *GET_DPI_FOR_SYSTEM {
+        return unsafe { f() } as f64 / 96.0;
     }
     1.0
 }
