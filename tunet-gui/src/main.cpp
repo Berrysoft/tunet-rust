91d27ba03acf3e53f65a20a24e62aff25624c4da
Berrysoft <Strawberry_Str@hotmail.com>
2021-11-06 15:57:13 +0800

    Fix qt5 default font on Windows.

--- tunet-gui/src/main.cpp
+++ tunet-gui/src/main.cpp
@@ -9,6 +9,12 @@ int main_impl(int argc, char** argv, Model* pmodel)
     QApplication app{ argc, argv };
     app.setApplicationDisplayName(u"清华校园网"_qs);
 
+#if defined(Q_OS_WIN) && QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
+    QFont f = app.font();
+    f.setFamily(u"Microsoft YaHei UI"_qs);
+    app.setFont(f);
+#endif
+
     MainWnd wnd{ pmodel };
     wnd.show();
 
