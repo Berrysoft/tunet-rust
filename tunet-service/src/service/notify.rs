5b86cc731a83ef7f5190e481184ed546882ce914
Berrysoft <Strawberry_Str@hotmail.com>
2023-04-05 13:44:11 +0800

    Ignore inactive session.

--- tunet-service/src/service/notify.rs
+++ tunet-service/src/service/notify.rs
@@ -1,18 +1,22 @@
 use std::{
+    ffi::c_void,
+    ops::Deref,
     os::windows::prelude::{AsRawHandle, FromRawHandle, OwnedHandle},
     ptr::null_mut,
 };
-use widestring::{u16str, U16CString, U16String};
+use tunet_helper::Result;
+use widestring::{u16cstr, U16CString};
 use windows::{
-    core::*,
+    core::{PCWSTR, PWSTR},
     Win32::{
         Foundation::HANDLE,
-        Security::{DuplicateTokenEx, SecurityIdentification, TokenPrimary, TOKEN_ALL_ACCESS},
         System::{
-            Environment::CreateEnvironmentBlock,
+            Environment::{CreateEnvironmentBlock, DestroyEnvironmentBlock},
             RemoteDesktop::{
-                WTSGetActiveConsoleSessionId, WTSQueryUserToken, WTSSendMessageW,
-                WTS_CURRENT_SERVER_HANDLE,
+                WTSActive, WTSConnectState, WTSEnumerateSessionsW, WTSFreeMemory,
+                WTSGetActiveConsoleSessionId, WTSQuerySessionInformationW, WTSQueryUserToken,
+                WTSSendMessageW, WTS_CONNECTSTATE_CLASS, WTS_CURRENT_SERVER_HANDLE,
+                WTS_SESSION_INFOW,
             },
             Threading::{
                 CreateProcessAsUserW, CREATE_UNICODE_ENVIRONMENT, PROCESS_INFORMATION,
@@ -23,67 +27,107 @@ use windows::{
     },
 };
 
+struct OwnedEnvironmentBlock(*mut c_void);
+
+impl Drop for OwnedEnvironmentBlock {
+    fn drop(&mut self) {
+        unsafe {
+            DestroyEnvironmentBlock(self.0).ok().ok();
+        }
+    }
+}
+
+struct OwnedSession(*mut WTS_SESSION_INFOW, u32);
+
+impl Deref for OwnedSession {
+    type Target = [WTS_SESSION_INFOW];
+
+    fn deref(&self) -> &Self::Target {
+        unsafe { std::slice::from_raw_parts(self.0, self.1 as _) }
+    }
+}
+
+impl Drop for OwnedSession {
+    fn drop(&mut self) {
+        unsafe {
+            WTSFreeMemory(self.0 as _);
+        }
+    }
+}
+
 pub fn notify() -> Result<()> {
     unsafe {
-        let session = WTSGetActiveConsoleSessionId();
-        let mut token = HANDLE::default();
-        WTSQueryUserToken(session, &mut token).ok()?;
-        let mut dup_token = HANDLE::default();
-        DuplicateTokenEx(
-            token,
-            TOKEN_ALL_ACCESS,
-            None,
-            SecurityIdentification,
-            TokenPrimary,
-            &mut dup_token,
-        )
-        .ok()?;
-        let dup_token = OwnedHandle::from_raw_handle(dup_token.0 as _);
-        let mut env = null_mut();
-        CreateEnvironmentBlock(&mut env, HANDLE(dup_token.as_raw_handle() as _), false).ok()?;
-        let mut si = STARTUPINFOW::default();
-        si.cb = std::mem::size_of_val(&si) as _;
-        si.dwFlags = STARTF_USESHOWWINDOW;
-        si.wShowWindow = SW_HIDE.0 as _;
-        let mut pi = PROCESS_INFORMATION::default();
-        let app_name =
-            U16CString::from_os_str(std::env::current_exe().unwrap().into_os_string()).unwrap();
-        // Need to set the first arg as the exe itself.
-        let mut command_line = U16CString::from_str("tunet-service.exe run-once").unwrap();
-        let app_dir =
-            U16CString::from_os_str(std::env::current_dir().unwrap().into_os_string()).unwrap();
-        CreateProcessAsUserW(
-            HANDLE(dup_token.as_raw_handle() as _),
-            PCWSTR(app_name.as_ptr()),
-            PWSTR(command_line.as_mut_ptr()),
-            None,
-            None,
-            false,
-            CREATE_UNICODE_ENVIRONMENT,
-            Some(env),
-            PCWSTR(app_dir.as_ptr()),
-            &si,
-            &mut pi,
-        )
-        .ok()?;
-        let _thread = OwnedHandle::from_raw_handle(pi.hThread.0 as _);
-        let _process = OwnedHandle::from_raw_handle(pi.hProcess.0 as _);
+        let mut buffer = null_mut();
+        let mut count = 0;
+        WTSEnumerateSessionsW(WTS_CURRENT_SERVER_HANDLE, 0, 1, &mut buffer, &mut count).ok()?;
+        let sessions = OwnedSession(buffer, count);
+        for session in &*sessions {
+            let session_id = session.SessionId;
+            let mut pstate: *mut WTS_CONNECTSTATE_CLASS = null_mut();
+            let mut bytesread = 0;
+            WTSQuerySessionInformationW(
+                WTS_CURRENT_SERVER_HANDLE,
+                session_id,
+                WTSConnectState,
+                &mut pstate as *mut _ as _,
+                &mut bytesread,
+            )
+            .ok()?;
+            let state = *pstate;
+            WTSFreeMemory(pstate as _);
+            if state != WTSActive {
+                continue;
+            }
+            let mut token = HANDLE::default();
+            if WTSQueryUserToken(session_id, &mut token).ok().is_err() {
+                continue;
+            }
+            let token = OwnedHandle::from_raw_handle(token.0 as _);
+            let mut env = null_mut();
+            CreateEnvironmentBlock(&mut env, HANDLE(token.as_raw_handle() as _), false).ok()?;
+            let env = OwnedEnvironmentBlock(env);
+            let mut si = STARTUPINFOW::default();
+            si.cb = std::mem::size_of_val(&si) as _;
+            si.dwFlags = STARTF_USESHOWWINDOW;
+            si.wShowWindow = SW_HIDE.0 as _;
+            let mut pi = PROCESS_INFORMATION::default();
+            let app_name = U16CString::from_os_str(std::env::current_exe()?.into_os_string())?;
+            // Need to set the first arg as the exe itself.
+            let mut command_line = U16CString::from_str("tunet-service.exe run-once")?;
+            let app_dir = U16CString::from_os_str(std::env::current_dir()?.into_os_string())?;
+            CreateProcessAsUserW(
+                HANDLE(token.as_raw_handle() as _),
+                PCWSTR(app_name.as_ptr()),
+                PWSTR(command_line.as_mut_ptr()),
+                None,
+                None,
+                false,
+                CREATE_UNICODE_ENVIRONMENT,
+                Some(env.0),
+                PCWSTR(app_dir.as_ptr()),
+                &si,
+                &mut pi,
+            )
+            .ok()?;
+            let _thread = OwnedHandle::from_raw_handle(pi.hThread.0 as _);
+            let _process = OwnedHandle::from_raw_handle(pi.hProcess.0 as _);
+        }
     }
     Ok(())
 }
 
 pub fn error(s: impl AsRef<str>) -> Result<()> {
-    let title = u16str!("tunet-service");
-    let msg = U16String::from_str(s.as_ref());
+    let title = u16cstr!("tunet-service");
+    let msg = U16CString::from_str(s.as_ref())?;
     let mut res = MESSAGEBOX_RESULT(0);
     unsafe {
         WTSSendMessageW(
             WTS_CURRENT_SERVER_HANDLE,
             WTSGetActiveConsoleSessionId(),
             PCWSTR(title.as_ptr()),
-            title.len() as _,
+            (title.len() * 2) as _,
             PCWSTR(msg.as_ptr()),
-            msg.len() as _,
+            (msg.len() * 2) as _,
             MB_OK,
             0,
             &mut res,
