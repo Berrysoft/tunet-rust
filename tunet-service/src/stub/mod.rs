1faa60c4d51a45f1502a51f8d48c94f669575062
Yuyi Wang <Strawberry_Str@hotmail.com>
2023-04-06 12:06:11 +0800

    Add service start for stub.

--- tunet-service/src/stub/mod.rs
+++ tunet-service/src/stub/mod.rs
@@ -1,3 +1,5 @@
+use tokio::signal::ctrl_c;
+use tokio_stream::StreamExt;
 use tunet_helper::{anyhow, Result};
 
 pub fn register(_interval: Option<humantime::Duration>) -> Result<()> {
@@ -8,6 +10,26 @@ pub fn unregister() -> Result<()> {
     Err(anyhow!("不支持的命令"))
 }
 
-pub fn start(_interval: Option<humantime::Duration>) -> Result<()> {
-    Err(anyhow!("不支持的命令"))
+pub fn start(interval: Option<humantime::Duration>) -> Result<()> {
+    tokio::runtime::Builder::new_current_thread()
+        .enable_all()
+        .build()?
+        .block_on(start_impl(interval))
+}
+
+async fn start_impl(interval: Option<humantime::Duration>) -> Result<()> {
+    let mut timer = crate::create_timer(interval);
+    loop {
+        tokio::select! {
+            _ = ctrl_c() => {
+                break;
+            }
+            _ = timer.next() => {
+                if let Err(msg) = crate::run_once(true).await {
+                    eprintln!("{}", msg)
+                }
+            }
+        }
+    }
+    Ok(())
 }
