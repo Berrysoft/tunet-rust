bb6c641326f28c9c53353c0c626a5a24648357eb
Berrysoft <Strawberry_Str@hotmail.com>
2023-04-30 14:14:02 +0800

    Try to add app user model id for Windows.

--- tunet-service/src/notification.rs
+++ tunet-service/src/notification.rs
@@ -4,7 +4,10 @@ use tunet_helper::{NetFlux, Result};
 pub fn succeeded(flux: NetFlux) -> Result<()> {
     #[cfg(target_os = "macos")]
     let _ = notify_rust::set_application("com.berrysoft.tunet");
-    Notification::new()
+    let mut notify = Notification::new();
+    #[cfg(target_os = "windows")]
+    notify.app_id("com.berrysoft.tunet");
+    notify
         .summary(&format!("登录成功：{}", flux.username))
         .body(&format!("流量：{}\n余额：{}", flux.flux, flux.balance))
         .show()?;
