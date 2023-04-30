68107dfe63397cd273235e0734eca0a2490e7bfd
Berrysoft <Strawberry_Str@hotmail.com>
2023-04-30 13:49:30 +0800

    Fix notification on mac in bundle.

--- tunet-service/src/notification.rs
+++ tunet-service/src/notification.rs
@@ -2,6 +2,8 @@ use notify_rust::Notification;
 use tunet_helper::{NetFlux, Result};
 
 pub fn succeeded(flux: NetFlux) -> Result<()> {
+    #[cfg(target_os = "macos")]
+    let _ = notify_rust::set_application("com.berrysoft.tunet");
     Notification::new()
         .summary(&format!("登录成功：{}", flux.username))
         .body(&format!("流量：{}\n余额：{}", flux.flux, flux.balance))
