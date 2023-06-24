e193e4c8ed47194f6ffefb21298387743df6258f
Yuyi Wang <Strawberry_Str@hotmail.com>
2023-06-25 01:39:38 +0800

    Use winresource.

--- tunet-gui/build.rs
+++ tunet-gui/build.rs
@@ -2,7 +2,7 @@ fn main() {
     slint_build::compile("ui/main.slint").unwrap();
     #[cfg(target_os = "windows")]
     if std::env::var("CARGO_CFG_TARGET_OS").as_deref() == Ok("windows") {
-        winres::WindowsResource::new()
+        winresource::WindowsResource::new()
             .set_icon("../logo.ico")
             .compile()
             .unwrap();
--- tunet/build.rs
+++ tunet/build.rs
@@ -1,7 +1,7 @@
 fn main() {
     #[cfg(target_os = "windows")]
     if std::env::var("CARGO_CFG_TARGET_OS").as_deref() == Ok("windows") {
-        winres::WindowsResource::new()
+        winresource::WindowsResource::new()
             .set_icon("../logo.ico")
             .compile()
             .unwrap();
