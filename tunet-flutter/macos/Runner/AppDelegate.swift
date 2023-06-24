802addb025f9cece966a0c2bd4b0c5abf6db9787
Yuyi Wang <Strawberry_Str@hotmail.com>
2023-06-24 20:12:41 +0800

    Add macos & linux project.

---
+++ tunet-flutter/macos/Runner/AppDelegate.swift
@@ -0,0 +1,10 @@
+import Cocoa
+import FlutterMacOS
+
+@NSApplicationMain
+class AppDelegate: FlutterAppDelegate {
+  override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+    print(dummy_method_to_enforce_bundling())
+    return true
+  }
+}
