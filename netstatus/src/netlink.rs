66ac6bd34e1197da54287c562462d98fce7cbc8a
Yuyi Wang <Strawberry_Str@hotmail.com>
2023-07-30 16:51:06 +0800

    Fix building with netlink.

--- netstatus/src/netlink.rs
+++ netstatus/src/netlink.rs
@@ -1,27 +1,17 @@
 use crate::*;
-use netlink_wi::{AttrParseError, NlSocket};
+use netlink_wi::NlSocket;
 
-type Result<T> = std::result::Result<T, AttrParseError>;
-
-fn current_impl() -> Result<NetStatus> {
-    if let Ok(sock) = NlSocket::connect() {
+pub fn current() -> NetStatus {
+    if let Ok(mut sock) = NlSocket::connect() {
         if let Ok(interfaces) = sock.list_interfaces() {
             for interface in interfaces {
-                let interface = interface?;
                 if let Some(ssid) = interface.ssid {
-                    return Ok(NetStatus::Wlan(ssid));
+                    return NetStatus::Wlan(ssid);
                 }
             }
         }
     }
-    Ok(NetStatus::Unknown)
-}
-
-pub fn current() -> NetStatus {
-    current_impl().unwrap_or_else(|e| {
-        log::warn!("{}", e);
-        NetStatus::Unknown
-    })
+    NetStatus::Unknown
 }
 
 pub fn watch() -> impl Stream<Item = ()> {
