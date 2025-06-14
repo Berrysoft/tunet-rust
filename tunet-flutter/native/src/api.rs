5e3134cb65d379e781069aa3b8561257485c8096
王宇逸 <Strawberry_Str@hotmail.com>
2025-06-14 17:08:22 +0800

    Fix api.rs

--- tunet-flutter/native/src/api.rs
+++ tunet-flutter/native/src/api.rs
@@ -196,16 +196,4 @@ impl Runtime {
     pub fn queue_status(&self, s: NetStatus) {
         self.queue(Action::Status(Some(s)));
     }
-
-    #[frb(sync)]
-    pub fn queue_connect(&self, ip: Ipv4AddrWrap) {
-        self.queue(Action::Connect(Ipv4Addr::from(ip.octets)));
-    }
-
-    #[frb(sync)]
-    pub fn queue_drop(&self, ips: Vec<Ipv4AddrWrap>) {
-        for ip in ips {
-            self.queue(Action::Drop(Ipv4Addr::from(ip.octets)));
-        }
-    }
 }
