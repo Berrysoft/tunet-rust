dffe7951c0965d4c21deaeb48f588e99a786038d
Yuyi Wang <Strawberry_Str@hotmail.com>
2025-10-21 01:09:08 +0800

    Fix warnings on Linux

--- netstatus/src/netlink.rs
+++ netstatus/src/netlink.rs
@@ -2,7 +2,7 @@ use crate::*;
 use netlink_wi::NlSocket;
 
 pub fn current() -> NetStatus {
-    if let Ok(mut sock) = NlSocket::connect() {
+    if let Ok(sock) = NlSocket::connect() {
         if let Ok(interfaces) = sock.list_interfaces() {
             for interface in interfaces {
                 if let Some(ssid) = interface.ssid {
