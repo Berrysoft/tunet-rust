a4f915e62a7d5ef572474ad93a99611be41665f4
Yuyi Wang <Strawberry_Str@hotmail.com>
2025-11-03 03:18:34 +0800

    Reset logger filter

--- tunet-flutter/native/src/api.rs
+++ tunet-flutter/native/src/api.rs
@@ -146,7 +146,7 @@ impl Runtime {
                 .with_max_level(log::LevelFilter::Trace)
                 .with_filter(
                     android_logger::FilterBuilder::new()
-                        .parse("warn,tunet=trace,native=trace,winio=trace")
+                        .parse("warn,tunet=trace,native=trace")
                         .build(),
                 ),
         );
