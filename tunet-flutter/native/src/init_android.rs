1f4521cb0fce47dd033e8ab95953026cd153cfe9
Yuyi Wang <Strawberry_Str@hotmail.com>
2026-06-04 01:44:41 +0800

    Fix rustls JNI

--- tunet-flutter/native/src/init_android.rs
+++ tunet-flutter/native/src/init_android.rs
@@ -1,13 +1,16 @@
 use jni::{
-    JNIEnv,
+    EnvUnowned,
+    errors::ThrowRuntimeExAndDefault,
     objects::{JClass, JObject},
 };
 
 #[unsafe(no_mangle)]
-pub extern "system" fn Java_io_github_berrysoft_tunet_1flutter_InitPlugin_init_1android(
-    mut env: JNIEnv,
-    _class: JClass,
-    context: JObject,
+pub extern "system" fn Java_io_github_berrysoft_tunet_1flutter_InitPlugin_init_1android<'caller>(
+    mut unowned_env: EnvUnowned<'caller>,
+    _class: JClass<'caller>,
+    context: JObject<'caller>,
 ) {
-    rustls_platform_verifier::android::init_hosted(&mut env, context).ok();
+    unowned_env
+        .with_env(|env| rustls_platform_verifier::android::init_with_env(env, context))
+        .resolve::<ThrowRuntimeExAndDefault>();
 }
