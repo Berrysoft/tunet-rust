5f3bd6f2fe3ca0d0dc5a50c7c468006fe8492cd5
Yuyi Wang <Strawberry_Str@hotmail.com>
2026-06-10 02:39:27 +0800

    Allow net on main thread

--- tunet-gui/android/app/src/main/java/io/github/berrysoft/tunet/MainActivity.java
+++ tunet-gui/android/app/src/main/java/io/github/berrysoft/tunet/MainActivity.java
@@ -1,9 +1,19 @@
 package io.github.berrysoft.tunet;
 
+import android.os.Bundle;
+import android.os.StrictMode;
+
 import rs.compio.winio.Activity;
 
 public class MainActivity extends Activity {
     static {
         System.loadLibrary("main");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitNetwork().build();
+        StrictMode.setThreadPolicy(policy);
     }
 }
